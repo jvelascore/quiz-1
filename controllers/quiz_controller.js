37620c6544b5c7bb274f86eb2783062c59bfd5d0
jvelascore <cbjuanve@gmail.com>
2015-05-08 10:43:47 +0200

    base de datos en heroku.2

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,11 +1,17 @@
+var models = require('../models/models.js');
+
 exports.question = function(req, res){
+	models.Quiz.findAll().success(function(quiz){
     res.render('quizes/question', {pregunta: 'capital de Italia'});
+  })
 };
 
 exports.answer = function(req, res){
-	if(req.query.respuesta === 'Roma'){
+   models.Quiz.findAll().success(function(quiz){}	
+	if(req.query.respuesta === quiz[0].respuesta){
 		res.render('quizes/answer', {respuesta: 'Correcto'});
 	}else{
 		res.render('quizes/answer', {respuesta: 'Incorrecto'})
 	}
+  })	
 };
