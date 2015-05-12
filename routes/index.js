c6b0b11cc731e1dd95ca5ad4a2906254e217dfcb
jvelascore <cbjuanve@gmail.com>
2015-05-12 10:41:42 +0200

    lista de preguntas.2

--- routes/index.js
+++ routes/index.js
@@ -10,7 +10,7 @@ router.get('/', function(req, res) {
 
 //router.get('/quizes/question', quizController.question);
 //router.get('/quizes/answer', quizController.answer);
-//router.get('/author', function(req, res) {
+
 router.get('/quizes', quizController.index);
 router.get('/quizes/:quizId(\\d+)', quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
