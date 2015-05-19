d86d9b09a29e908123cf8fe8af94e41a2b3477fd
jvelascore <cbjuanve@gmail.com>
2015-05-19 19:15:36 +0200

    moderacion de comentarios

--- routes/index.js
+++ routes/index.js
@@ -15,6 +15,7 @@ router.get('/', function(req, res) {
 
 //Autoload de comandos con :quizId
 router.param('quizId', quizController.load); //autoload :quizId
+router.param('commentId', commentController.load);//autoload :commentId
 
 //Definicion de rutas de sesion
 router.get('/login', sessionController.new);//formulario login
@@ -37,8 +38,11 @@ router.get('/quizes/:quizId(\\d+)/edit',   sessionController.loginRequired, quiz
 router.put('/quizes/:quizId(\\d+)',        sessionController.loginRequired, quizController.update);
 router.delete('/quizes/:quizId(\\d+)',     sessionController.loginRequired, quizController.destroy);
 
+//Definicion de rutas de comentarios
 router.get('/quizes/:quizId(\\d+)/comments/new', commentController.new);
 router.post('/quizes/:quizId(\\d+)/comments', commentController.create);
+router.get('/quizes/:quizId(\\d+)/comments/:commentId(\\d+)/publish',
+                                  sessionController.loginRequired, commentController.publish);
 //  router.get('/author', function(req, res) {
 //   res.render('author', {title: 'creditos',  errors: [] });
 //});
